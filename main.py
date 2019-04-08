f0d90e3b6c7fdb6ec08a6deaaabf8cf7a18ac814
Danil Ginzburg <danilgin@inbox.ru>
2019-04-08 18:17:47 +0300

    good code style

--- main.py
+++ main.py
@@ -10,116 +10,131 @@ import argparse
 IMAGE_SIZE = (256, 256)
 mutation_chance = 0.2
 num_genes = 2500
-num_generations = 50
+num_generations = 20
 gene_deviation = 16
 location_mute = (-4, 4)  # define borders for location mutation
 alpha = 0.5
+src_filename = "image_test/mona.jpg"
+
+
+class Point:
+
+    def __init__(self, x=None, y=None, rand=False, from_point=None):
+        if from_point is not None:
+            x = int(from_point.x + random.randint(-gene_deviation, gene_deviation + 1))
+            if x < 0:
+                x = 0
+            if x > IMAGE_SIZE[0]:
+                x = IMAGE_SIZE[0]
+            y = int(from_point.y + random.randint(-gene_deviation, gene_deviation + 1))
+            if y < 0:
+                y = 0
+            if y > IMAGE_SIZE[1]:
+                y = IMAGE_SIZE[1]
+        elif rand:
+            x = random.randint(0, IMAGE_SIZE[0]-1)
+            y = random.randint(0, IMAGE_SIZE[0]-1)
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def get_center(*points, location=None):
+        if location is not None:
+            points = location.points
+        x = sum(map(lambda point: point.x, points)) // len(points)
+        y = sum(map(lambda point: point.y, points)) // len(points)
+        if x > 255:
+            x = 255
+        if y > 255:
+            y = 255
+        return Point(x, y)
+
+
+class Color:
+
+    def __init__(self, r=None, g=None, b=None):
+        if r is None:
+            r, g, b = [random.randint(0, 255) for _ in range(3)]
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def pure(self):
+        return self.r, self.g, self.b
+
+
+class Location:
+
+    def __init__(self, points=None):
+        if points is not None:
+            self.points = points
+        else:
+            self.points = list()
+
+    def __iadd__(self, point):
+        assert type(point) == Point, 'Can not add non-point'
+        self.points.append(point)
+        return Location(points=self.points)
+
+    def __getitem__(self, key):
+        return self.points[key]
+
+    def pure(self):
+        return [(point.y, point.x) for point in self.points]
 
 
 class Gene:
 
     def __init__(self):
-        self.color = [random.randint(0, 255) for _ in range(3)]
-        x, y = (random.randint(0, IMAGE_SIZE[0]), random.randint(0, IMAGE_SIZE[1]))
-        self.location = list()
+        self.color = Color()
+        self.location = Location()
+
+        center_point = Point(rand=True)  # random point
         for _ in range(3):
-            ptx = int(x + random.randint(-gene_deviation, gene_deviation+1))
-            if ptx < 0:
-                ptx = 0
-            if ptx > IMAGE_SIZE[0]:
-                ptx = IMAGE_SIZE[0]
-            pty = int(y + random.randint(-gene_deviation, gene_deviation+1))
-            if pty < 0:
-                pty = 0
-            if pty > IMAGE_SIZE[1]:
-                pty = IMAGE_SIZE[1]
-            self.location.append((ptx, pty))
+            point = Point(from_point=center_point)
+            self.location += point
 
     def mutate(self):
 
-        y = (self.location[0][0] + self.location[1][0] + self.location[2][0]) // 3
-        x = (self.location[0][1] + self.location[1][1] + self.location[2][1]) // 3
+        center = Point.get_center(location=self.location)
 
         if random.random() < mutation_chance:
             # mutate color
-            if x > 255:
-                x = 255
-            if y > 255:
-                y = 255
-            r, g, b = image[x, y]
-            self.color = (int(r), int(g), int(b))
+            r, g, b = image[center.x, center.y]
+            self.color = Color(int(r), int(g), int(b))
 
         if random.random() < mutation_chance:
             # mutate location
-            pt1 = [0, 0]
-            pt1[0] = self.location[0][0] + random.randint(*location_mute)
-            if pt1[0] < 0:
-                pt1[0] = 0
-            if pt1[0] > IMAGE_SIZE[0]:
-                pt1[0] = IMAGE_SIZE[0]
-            elif pt1[0] - y > gene_deviation:
-                pt1[0] = y + gene_deviation
-            elif pt1[0] - y < -gene_deviation:
-                pt1[0] = y - gene_deviation
+            location = Location()
 
-            pt1[1] = self.location[0][1] + random.randint(*location_mute)
-            if pt1[1] < 0:
-                pt1[1] = 0
-            if pt1[1] > IMAGE_SIZE[1]:
-                pt1[1] = IMAGE_SIZE[1]
-            elif pt1[1] - x > gene_deviation:
-                pt1[1] = x + gene_deviation
-            elif pt1[1] - x < -gene_deviation:
-                pt1[1] = x - gene_deviation
+            for i in range(3):
+                # mutate one point
+                pt = Point()
+                pt.y = self.location[i].y + random.randint(*location_mute)
+                if pt.y < 0:
+                    pt.y = 0
+                if pt.y > IMAGE_SIZE[0]:
+                    pt.y = IMAGE_SIZE[0]
+                elif pt.y - center.y > gene_deviation:
+                    pt.y = center.y + gene_deviation
+                elif pt.y - center.y < -gene_deviation:
+                    pt.y = center.y - gene_deviation
 
-            pt2 = [0, 0]
-            pt2[0] = self.location[1][0] + random.randint(*location_mute)
-            if pt2[0] < 0:
-                pt2[0] = 0
-            if pt2[0] > IMAGE_SIZE[0]:
-                pt2[0] = IMAGE_SIZE[0]
-            elif pt2[0] - y > gene_deviation:
-                pt2[0] = y + gene_deviation
-            elif pt2[0] - y < -gene_deviation:
-                pt2[0] = y - gene_deviation
+                pt.x = self.location[i].x + random.randint(*location_mute)
+                if pt.x < 0:
+                    pt.x = 0
+                if pt.x > IMAGE_SIZE[1]:
+                    pt.x = IMAGE_SIZE[1]
+                elif pt.x - center.x > gene_deviation:
+                    pt.x = center.x + gene_deviation
+                elif pt.x - center.x < -gene_deviation:
+                    pt.x = center.x - gene_deviation
 
-            pt2[1] = self.location[1][1] + random.randint(*location_mute)
-            if pt2[1] < 0:
-                pt2[1] = 0
-            if pt2[1] > IMAGE_SIZE[1]:
-                pt2[1] = IMAGE_SIZE[1]
-            elif pt2[1] - x > gene_deviation:
-                pt2[1] = x + gene_deviation
-            elif pt2[1] - x < -gene_deviation:
-                pt2[1] = x - gene_deviation
-
-            pt3 = [0, 0]
-            pt3[0] = self.location[2][0] + random.randint(*location_mute)
-            if pt3[0] < 0:
-                pt3[0] = 0
-            if pt3[0] > IMAGE_SIZE[0]:
-                pt3[0] = IMAGE_SIZE[0]
-            elif pt3[0] - y > gene_deviation:
-                pt3[0] = y + gene_deviation
-            elif pt3[0] - y < -gene_deviation:
-                pt3[0] = y - gene_deviation
-
-            pt3[1] = self.location[2][1] + random.randint(*location_mute)
-            if pt3[1] < 0:
-                pt3[1] = 0
-            if pt3[1] > IMAGE_SIZE[1]:
-                pt3[1] = IMAGE_SIZE[1]
-            elif pt3[1] - x > gene_deviation:
-                pt3[1] = x + gene_deviation
-            elif pt3[1] - x < -gene_deviation:
-                pt3[1] = x - gene_deviation
-        else:
-            pt1, pt2, pt3 = self.location
-
-        self.location = [tuple(pt1), tuple(pt2), tuple(pt3)]
+                location += pt
+            self.location = location
 
     def draw(self, overlay):
-        cv2.drawContours(overlay, [np.array(self.location)], 0, self.color, -1)
+        cv2.drawContours(overlay, [np.array(self.location.pure())], 0, self.color.pure(), -1)
 
 
 class Chromosome:
@@ -158,19 +173,12 @@ class Chromosome:
 class GeneticImage:
 
     def __init__(self):
-        self.population = [Chromosome() for _ in range(2)]
+        self.population = [Chromosome(), Chromosome()]
 
     def generate_population(self):
         for generation in tqdm(range(num_generations), unit='generation'):
-            child1 = self.population[0] + self.population[1]
-            child2 = self.population[0] + self.population[1]
-            child3 = self.population[0] + self.population[1]
-            if child3.fitness < child2.fitness and child3.fitness < child1.fitness:
-                self.population = [child1, child2]
-            elif child1.fitness < child2.fitness and child1.fitness < child3.fitness:
-                self.population = [child2, child3]
-            elif child2.fitness < child1.fitness and child2.fitness < child3.fitness:
-                self.population = [child1, child3]
+            children = [self.population[0] + self.population[1] for _ in range(3)]
+            self.population = sorted(children, key=lambda x: x.fitness)[:2]
             yield self.population
 
     @staticmethod
@@ -189,13 +197,13 @@ def main():
         cv2.waitKey(1)
 
     im = cv2.resize(im, (512, 512))
-    cv2.imwrite('result', im)
-    cv2.waitKey()
+    out_filename = src_filename.split('.')[0] + '_result.jpg'
+    cv2.imwrite(out_filename, im)
+    print(f'Done. Check result in {out_filename}')
 
 
 if __name__ == '__main__':
 
-    src_filename = "image_test/loli.jpg"
     parser = argparse.ArgumentParser()
     parser.add_argument("--path")
     parser.add_argument("--generations")
